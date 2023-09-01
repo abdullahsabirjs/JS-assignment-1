66861a9e58eea1a33ca4257aa7aaf1e462458130
abdullah <your.email@example.com>
2023-09-01 22:22:29 +0500

    index.html added

---
+++ script.js
@@ -0,0 +1,64 @@
+// ! chap 1
+// ? Q 1
+// * alert("Hello, welcome to our website!");
+
+// ? Q 2
+// * var message = "This is an alert assignment";
+//  *   document.write(message);
+
+// ? Q 3
+// * var message = "This is an alert.\n\nIt has line breaks.";
+// * alert(message);
+
+// ? Q 4
+// * alert("Welcome to JS land!");
+// * var confirmation = confirm("Prevent this page from creating additional dialogs?");
+
+//  ? Q 5
+// * var message = "Hello... I can run JS through my web browser console";
+// * console.log(message);
+// * alert(message);
+  
+// ? Q 6
+// * In html File
+
+
+
+// ! chap 2
+// ? Q 1, 2, 3, 4
+// 1 and 2
+// * var username;
+// * var myName = "Your Full Name";
+
+// 3
+// * var message; 
+// * message = "Hello World";
+// * alert(message);
+
+//  4
+// * var studentName = "John Doe";
+// * var age = 20;
+// * var grade = "A";
+// * var bioData = "Student Name: " + studentName + "\nAge: " + age + "\nGrade: " + grade;
+// * alert(bioData);
+
+// ? Q 5
+// * var word = "pizza";
+// * for (var i = word.length; i > 0; i--) {
+// *     alert(word.slice(0, i));
+// * }
+
+// ? Q 6
+// * var email = "example@example.com";
+// * alert("My email address is: " + email);
+
+// ? Q 7
+// * var book = "A smarter way to learn JavaScript";
+// * alert("I am currently reading the book: " + book);
+
+// ? Q 8, 9
+// * document.write("This is displayed in the browser through JavaScript.");
+// * var specialString = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// * alert(specialString);
+// * document.write("<br>" + specialString);
+
